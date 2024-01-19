d584708793971b82c88ea9347780ecc25efc4652
chirprush <chirpyboat73@gmail.com>
2024-01-19 14:03:09 -0500

    Update templates for different contests

---
+++ templates/template.py
@@ -0,0 +1,7 @@
+def read_some():
+    return [int(i) for i in input().split(" ")]
+
+t = int(input())
+
+for _ in range(t):
+    pass
--- templates/template_usaco.cpp
+++ templates/template_usaco.cpp
@@ -2,27 +2,37 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <set>
 #include <algorithm>
+
 #include <cmath>
+#include <cstdio>
 
 typedef long long ll;
 
 template<typename T>
-void read_some(std::vector<T> &v, int N) {
+void read_some(std::ifstream &fin, std::vector<T> &v, int N) {
     for (int i = 0; i < N; i++) {
         T a;
-        std::cin >> a;
+        fin >> a;
         v.push_back(a);
     }
 }
 
 template<typename T>
 void print_some(std::vector<T> &v) {
+    std::cout << "[";
+
     for (int i = 0; i < v.size(); i++) {
-        std::cout << v[i] << std::endl;
+        std::cout << " " << v[i];
     }
+    
+    std::cout << " ]" << std::endl;
 }
 
 int main() {
+    freopen("filename.in", "r", stdin);
+    freopen("filename.out", "w", stdout);
+
     return 0;
 }
